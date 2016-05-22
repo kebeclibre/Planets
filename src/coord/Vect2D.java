737514334deb5fb6f655a5a67eb405d3c0aed44f
kebeclibre <lcsperais@gmail.com>
2016-05-22 07:01:48 +0200

    init vect

---
+++ src/coord/Vect2D.java
@@ -0,0 +1,25 @@
+package coord;
+
+public class Vect2D {
+	private double x;
+	private double y;
+	
+	public double getX() {
+		return x;
+	}
+	public void setX(double x) {
+		this.x = x;
+	}
+	public double getY() {
+		return y;
+	}
+	public void setY(double y) {
+		this.y = y;
+	}
+	
+	public void addVectAccel(Vect2D accel) {
+		this.x += accel.x;
+		this.y += accel.y;
+	}
+	
+}
